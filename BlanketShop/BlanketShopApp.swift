b0bf9169cd38e04cbbf8b9df26eab7ba4d26af60
Zhanet <zanet777nikolovska@gmail.com>
2024-04-29 00:34:21 +0200

    Initial Commit

---
+++ BlanketShop/BlanketShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlanketShopApp.swift
+//  BlanketShop
+//
+//  Created by Zhanet Nikolovska on 29.4.24.
+//
+
+import SwiftUI
+
+@main
+struct BlanketShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
